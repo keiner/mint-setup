7ec73dfae99e124ca2c91ac194d192eab4057812
keiner <nereos1@gmx.de>
2018-02-27 18:57:39 +0100

    cleanup

---
+++ 10_config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+###OH-MY-ZSH
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo chmod +s /sbin/shutdown 
+
+
+sudo chsh -s /bin/zsh
+
+######CONFIG
+mkdir -p /home/$USER/cfg
+cd ~
+git clone https://github.com/keiner/cfg.git
+ln -f -s /home/$USER/cfg/.vimrc ~/.vimrc
+ln -f -s /home/$USER/cfg/.zshrc ~/.zshrc
+ln -f -s /home/$USER/cfg/.aliasesrc ~/.aliasesrc
+ln -f -s /home/$USER/cfg/.bashrc ~/.bashrc
+ln -f -s /home/$USER/cfg/.tmux.conf ~/.tmux.conf
+ln -f -s /home/$USER/cfg/.vimperatorrc ~/.vimperatorrc
+
