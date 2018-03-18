cc506823ef6f2f3ec619aca82e2e840ca3c6df61
keiner <nereos1@gmx.de>
2018-03-18 17:45:44 +0100

    vim from source now using vundle

--- 30_vimsetup.sh
+++ 30_vimsetup.sh
@@ -1,18 +1,48 @@
-#!/bin/sh #### VIM install 
+#!/bin/sh
+
+#Install vim from source
+#before starting make sure u uninstall all preinstalled vim stuff
+
+#First, install all the prerequisite libraries
 sudo apt install -y \
-	vim \
-	vim-latexsuite \
-	vim-addon-manager \
-	vim-youcompleteme \
-	texlive-full
+	libncurses5-dev libgnome2-dev libgnomeui-dev \
+	libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+	libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+	python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git \
+	checkinstall
 
-vim-addons install latex-suite
-vim-addons install youcompleteme
 
-###VIM Plugins
-mkdir -p ~/.vim/bundle
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/nerdcommenter.git
-git clone git://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/majutsushi/tagbar.git
+#clone the sourcecode
+mkdir -p ~/work/source
+cd ~/work/source
+git clone https://github.com/vim/vim.git
+#configure it
+cd vim
+./configure --with-features=huge \
+	--enable-multibyte \
+	--enable-rubyinterp=yes \
+	--enable-pythoninterp=yes \
+	--with-python-config-dir=/usr/lib/python2.7/config \
+	--enable-python3interp=no\
+	--with-python3-config-dir=/usr/lib/python3.5/config \
+	--enable-perlinterp=yes \
+	--enable-luainterp=yes \
+	--enable-gui=gtk2 \
+	--enable-cscope \
+	--prefix=/usr/local
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
+#for later easy uninstall use checkinstall
+sudo checkinstall
+#install vim on ur system
+sudo make install
+
+#Set vim as ur default editor
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
+sudo update-alternatives --set editor /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
+sudo update-alternatives --set vi /usr/local/bin/vim
+
+#now start vim and use :PluginInstall
+#to install YCM-core do
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --all
